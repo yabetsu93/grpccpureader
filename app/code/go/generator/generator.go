acb5986e88e956cce21a997ff3dcb3ff51ae22b5
yabetsu93 <jabpau93>
2020-04-29 16:47:25 +0900

    renamed and modify code

--- app/code/go/generator/generator.go
+++ app/code/go/generator/generator.go
@@ -1,15 +1,100 @@
 package generator
 
+import (
+	"grpccpureader/protos/pb/go/cpu"
+	"grpccpureader/protos/pb/go/keyboard"
+	"grpccpureader/protos/pb/go/memory"
+	"grpccpureader/protos/pb/go/storage"
+)
 
-
-func NewKeyboard() *keyboard. {
+// NewKeyboard returns new keyboad
+func NewKeyboard() *keyboard.Keyboard {
 	keyboard := &keyboard.Keyboard{
-		Layout:  randomKeyboardLayout,
+		Layout:  randomKeyboardLayout(),
 		Backlit: randomBool(),
 	}
 	return keyboard
 }
 
+// NewCPU returns new cpu
 func NewCPU() *cpu.CPU {
-	cpu.
+	brand := randomCPUBrand()
+	name := randomCPUName(brand)
+	numberCores := randomInt(2, 8)
+	numberThreads := randomInt(numberCores, 12)
+	minGHZ := randomFloat64(2.0, 3.5)
+	maxGHZ := randomFloat64(minGHZ, 5.0)
+
+	cpu := &cpu.CPU{
+		ProcessUnitInfo: *cpu.ProcessUnitInfo{
+			Brand:  brand,
+			Name:   name,
+			MinGhz: minGHZ,
+			MaxGhz: maxGHZ,
+		},
+		NumberCores:   uint32(numberCores),
+		NumberThreads: uint32(numberThreads),
+	}
+
+	return cpu
+}
+
+// NewGPU returns new gpu
+func NewGPU() *cpu.GPU {
+	brand := randomCPUBrand()
+	name := randomCPUName(brand)
+	minGHZ := randomFloat64(1.0, 1.5)
+	maxGHZ := randomFloat64(minGHZ, 2.0)
+	memory := &memory.Memory{
+		Value: uint64(randomInt(2, 6)),
+		Unit:  memory.Memory_GIGABYTE,
+	}
+
+	gpu := &cpu.GPU{
+		ProcessUnitInfo: &cpu.ProcessUnitInfo{
+			Brand:  brand,
+			Name:   name,
+			MinGhz: minGHZ,
+			MaxGhz: maxGHZ,
+		},
+		Memory: memory,
+	}
+
+	return gpu
+}
+
+// NewRam returns a new ram
+func NewRam() *memory.Memory {
+	ram := &memory.Memory{
+		Value: uint64(randomInt(2, 6)),
+		Unit:  memory.Memory_GIGABYTE,
+	}
+
+	return ram
+}
+
+// NewSSD returns a new ssd
+func NewSSD() *storage.Storage {
+	ssd := &storage.Storage{
+		Driver: storage.Storage_SSD,
+		Memory: &memory.Memory{
+			Value: uint64(randomInt(128, 1024)),
+			Unit:  memory.Memory_GIGABYTE,
+		},
+	}
+
+	return ssd
+}
+
+// NewHDD returns a new hdd
+func NewHDD() *storage.Storage {
+	hdd := &storage.Storage{
+		Driver: storage.Storage_HDD,
+		Memory: &memory.Memory{
+			Value: uint64(randomInt(128, 1024)),
+			Unit:  memory.Memory_GIGABYTE,
+		},
+	}
+
+	return hdd
 }
--- app/code/go/generator/random.go
+++ app/code/go/generator/random.go
@@ -19,3 +19,42 @@ func randomKeyboardLayout() pb.Keyboard_Layout {
 func randomBool() bool {
 	return rand.Intn(2) == 1
 }
+
+func randomCPUBrand() string () {
+	return randomStringFromSet("Intel", "AMD")
+}
+
+func randomStringFromSet(a ...string) string {
+	n = len(a)
+	if n == 0 {
+		return ""
+	}
+	return a[rand.Intn(n)]
+}
+
+func randomCPUName(brand string) string {
+	switch(brand) {
+	case 1:
+		"Intel": 
+		return randomStringFromSet(
+			"Xeon E-2286M",
+			"Core i9-9980HK",
+			"Core i9-9750H",
+			"Core i9-9400F",
+		)
+	default:
+		return randomStringFromSet(
+			"Ryzen 7 PRD 2700U",
+			"Ryzen 7 PRD 3500U",
+			"Ryzen 7 PRD 4300U",
+		)
+	}
+}
+
+func randomInt(min, max int) int {
+	return min + rand.Intn(max-min+1)
+}
+
+func randomFloat64(min, max float64) float64 {
+	return min + rand.Float64() * (max - min)
+}
